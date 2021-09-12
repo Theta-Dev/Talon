345811f72b36a48961b3ec0160f0caf4ecd78243
Theta-Dev <t.testboy@gmail.com>
2021-09-12 23:20:08 +0200

    Added global stylesheets

--- ui/menu/src/main.ts
+++ ui/menu/src/main.ts
@@ -1,7 +1,8 @@
-import App from './App.svelte'
+import App from "./App.svelte"
+import "./style/main.sass"
 
 const app = new App({
-  target: document.getElementById('talon-sidebar-container')
+  target: document.getElementById("talon-sidebar-container")
 })
 
 export default app
