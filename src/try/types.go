4e1810c7c30bdc2c5f3552ab674f39d69f15e655
Theta-Dev <t.testboy@gmail.com>
2021-10-01 18:55:55 +0200

    Add gotry

---
+++ src/try/types.go
@@ -0,0 +1,13 @@
+package try
+
+// gotry auto-generated type definitions. DO NOT EDIT.
+
+import (
+	"gorm.io/gorm"
+)
+
+// DB is a helper method to handle errors of func() (*gorm.DB, error) functions
+func DB(v *gorm.DB, err error) (*gorm.DB) {
+	Check(err)
+	return v
+}
